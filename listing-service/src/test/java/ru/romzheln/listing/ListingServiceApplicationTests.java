71270ebc581b1a70c69bc439d35fc46a345c7d43
ZhelnovachevRoman <zhelnovachev@gmail.com>
2026-06-27 09:44:05 +0300

    Initial commit

---
+++ listing-service/src/test/java/ru/romzheln/listing/ListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.romzheln.listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
