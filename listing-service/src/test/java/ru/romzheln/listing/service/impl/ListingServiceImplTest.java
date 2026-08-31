6ad1d38ba1d21090d03d3bdfa9ecbe50fe7e5e22
ZhelnovachevRoman <zhelnovachev@gmail.com>
2026-08-31 11:16:29 +0300

    Пишу инфраструктуру для search 2

--- listing-service/src/test/java/ru/romzheln/listing/service/impl/ListingServiceImplTest.java
+++ listing-service/src/test/java/ru/romzheln/listing/service/impl/ListingServiceImplTest.java
@@ -1,10 +1,33 @@
 package ru.romzheln.listing.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.romzheln.listing.mapper.ListingMapper;
+import ru.romzheln.listing.repository.ListingRepository;
+import ru.romzheln.listing.service.ListingService;
+import ru.romzheln.listing.service.OutboxEventService;
+import ru.romzheln.listing.service.PropertyService;
 
+@ExtendWith(MockitoExtension.class)
 class ListingServiceImplTest {
+
+    @Mock
+    private ListingRepository listingRepository;
+
+    @Mock
+    private OutboxEventService outboxEventService;
+
+    @Mock
+    private ListingMapper listingMapper;
+
+    @Mock
+    private PropertyService propertyService;
+
+    @InjectMocks
+    private ListingService listingService;
 
     @Test
     void createListing() {
